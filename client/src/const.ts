8a2c3824e09632f16c173380b05f7341aa9059d1
sabahat124 <78961524+sabahat124@users.noreply.github.com>
2026-05-16 22:44:24 +0500

    Fix server ports and links

--- client/src/const.ts
+++ client/src/const.ts
@@ -1,4 +1,4 @@
 export const getLoginUrl = () => {
   // We are hard-coding this to bypass the "Invalid URL" crash entirely
-  return "http://localhost:https://idyllic-melomakarona-96282b.netlify.app/login";
+  return "https://socialmind-app-production.up.railway.app";
 };
